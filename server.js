6c22d09f436df5995d449c66d4dac34edaed5861
CapraRoyale <siege.lehman@gmail.com>
2019-11-23 10:18:45 -0800

    Tweaking the comments to try and solve data passthrough

--- server.js
+++ server.js
@@ -130,14 +130,15 @@ app.get("/articles", function (req, res) {
 app.get("/articles/:id", function (req, res) {
     // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
     db.Article.findOne({
-            _id: req.params.id
-        })
+        _id: req.params.id
+    })
         // ..and populate all of the notes associated with it
         .populate("note")
         .then(function (dbArticle) {
             // If we were able to successfully find an Article with the given id, send it back to the client
             // res.json(dbArticle);
-            console.log(dbArticle);
+            // console.log(dbArticle);
+
             res.render("viewArticle", {
                 article: dbArticle
             })
